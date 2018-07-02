10d1382d0cce7914502f03a59f7e64a3b3459197
Chris Franklin <cf73021@gmail.com>
2018-07-02 20:00:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,14 @@
-
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken(board, index)
+    true
+  elsif position_taken?(board, index)
+    false
+  elsif index.between?(0,8) == false
+    false
+  else
+    false
+  end
+end
 
 def position_taken?(board, index)
   board[index] == "X" || board[index] == "O"
