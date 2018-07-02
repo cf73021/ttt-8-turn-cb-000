66946617cfa334b2fbfffbddb46f4f89e87961c4
Chris Franklin <cf73021@gmail.com>
2018-07-02 19:53:47 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,38 @@
+def valid_move?(board, index)
+  index <= 8 && index >= 0 == true &&
+  position_taken?(board, index) == false
+end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def move(board, index, value = "X")
+  valid_move?(board, index)
+  board[index] = value
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, value = "X")
+    display_board(board)
+  else
+    puts "Please enter 1-9:"
+    index = gets.strip
+  end
+end
