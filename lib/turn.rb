387b003f27e85a2eb715248884808f8ed168a2a2
Chris Franklin <cf73021@gmail.com>
2018-07-02 20:02:53 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,13 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  board[index] == "X" || board[index] == "O"
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
 end
 
 def input_to_index(input)
